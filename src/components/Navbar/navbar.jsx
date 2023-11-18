2cb9ac9d3c5df5d158b3e56a195779f4dd777f21
BifinJ <jamesbifin@gmail.com>
2023-11-18 09:48:46 +0530

    Dt. 18/11 Tym. 9:48am

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
+import { Nav } from "react-bootstrap";
 
 export default function Navbar() {
+  const [Nav, setNav] = useState(true);
+  const setNavigation = () => {
+    setNav(!Nav);
+  };
+
   return (
-    <>
-      <nav className="navigation">
+    <nav>
+      <section className="navigation">
         <div className="logo">LOGO</div>
+        <i
+          class={`bi bi-list ${Nav ? "bi-active" : ""}`}
+          onClick={setNavigation}
+        ></i>
+        <i
+          class={`bi bi-x ${Nav ? "" : "bi-active"}`}
+          onClick={setNavigation}
+        ></i>
         <div className="navigation-links">
           <ul>
             <li>Home</li>
@@ -16,7 +30,7 @@ export default function Navbar() {
             <li className="register">Register</li>
           </ul>
         </div>
-      </nav>
-    </>
+      </section>
+    </nav>
   );
 }
