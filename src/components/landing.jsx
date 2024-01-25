e6a8ba30c77e33255567e56d81f7910fe51d5e75
BifinJ <jamesbifin@gmail.com>
2024-01-25 20:44:47 +0530

    idea submission

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import Hero from './hero';
-import About from './about';
-import Previous from './previous/previous_year';
-import FAQPage from './FAQ/FAQ';
-import Contact from './contact/contact';
-import Footer from './Footer/Footer';
-import Dash from './dash/dash';
-import DashInd from './dash/dashInd';
-export default function landing()
-{
-    return(<>
-         <Hero />
+import React from "react";
+import Hero from "./hero";
+import About from "./about";
+import Previous from "./previous/previous_year";
+import FAQPage from "./FAQ/FAQ";
+import Contact from "./contact/contact";
+import Footer from "./Footer/Footer";
+import Dash from "./dash/dash";
+import DashInd from "./dash/dashInd";
+import IdeaSubmission from "./IdeaSubmission/IdeaSubmission";
+export default function landing() {
+  return (
+    <>
+      <Hero />
       <About />
       <Previous />
-      <FAQPage/>
+      <FAQPage />
       <Contact />
       <Footer />
-      <DashInd/>
-    </>);
+      <DashInd />
+      <IdeaSubmission />
+    </>
+  );
 }
